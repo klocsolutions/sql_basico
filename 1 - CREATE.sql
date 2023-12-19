f087c2f2f56057551f456038dd5fd4ad3ef98d56
klocsolutions <154376319+klocsolutions@users.noreply.github.com>
2023-12-19 15:00:30 -0600

    Update 1 - CREATE.sql

--- 1 - CREATE.sql
+++ 1 - CREATE.sql
@@ -1,4 +1,4 @@
-Video Youtube: https://youtu.be/R1xEG3mopMo?si=Hy68m2gRQiZEJzen
+-- Video Youtube: https://youtu.be/R1xEG3mopMo?si=Hy68m2gRQiZEJzen
 -- Crear tabla de empleados
 CREATE TABLE Empleados (
     Codigo int, 
